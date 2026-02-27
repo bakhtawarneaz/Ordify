8c490bf36eb90510ca10d546cde7cc7d7c22b467
bakhtawarneaz <bakhtawarneaz@gmail.com>
2026-02-27 09:35:44 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -8,19 +8,11 @@ const { seedRoles } = require('./utils/roleSeeder');
 const authRoutes = require('./routes/auth.routes');
 const menuRoutes = require('./routes/menu.routes');
 const userPermissionRoutes = require('./routes/userPermission.routes');
-const chatbotFlowRoutes = require('./routes/chatbotFlow.routes');
-const flowNodeRoutes = require('./routes/flowNode.routes');
-const flowConnectionRoutes = require('./routes/flowConnection.routes');
-const chatbotSessionRoutes = require('./routes/chatbotSession.routes');
 
 fastify.register(cors, { origin: '*' });
 fastify.register(authRoutes, { prefix: '/api/auth' });
 fastify.register(menuRoutes, { prefix: '/api/menu' });
 fastify.register(userPermissionRoutes, { prefix: '/api/permission' });
-fastify.register(chatbotFlowRoutes, { prefix: '/api/chatbotFlow' });
-fastify.register(flowNodeRoutes, { prefix: '/api/chatbotNode' });
-fastify.register(flowConnectionRoutes, { prefix: '/api/chatbotConnection' });
-fastify.register(chatbotSessionRoutes, { prefix: '/api/chatbotSession' });
 
 // DB Connection
 sequelize.sync({ alter: true })
